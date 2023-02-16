11a2052fcf9ba37a794be28d89d213b88425a4a4
Gustavo Bautista <bgustavo81@gmail.com>
2023-02-16 11:11:25 -0500

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,9 +18,9 @@ const Card = ({ _id, name, prompt, photo }) => (
           <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">{name[0]}</div>
           <p className="text-white text-sm">{name}</p>
         </div>
-        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
+        {/* <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
           <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
-        </button>
+        </button> */}
       </div>
     </div>
   </div>
